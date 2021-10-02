e9580baeda0cb93282a7a802e22c9073cca443b7
Razvan Gheorghe Prajanu <71783091+RazvanPRA@users.noreply.github.com>
2021-10-02 18:11:03 +0300

    Revert "Feature/constants"

--- scr/constants/COLORS.js
+++
@@ -1,9 +0,0 @@
-const COLORS = {
-  header: "#1A4B90",
-  water: "#357EBD",
-  rock: "#78480F",
-  air: "#02FDC1",
-  fire: "#E86318",
-  white: "#FFFFFF",
-};
-export default COLORS;
--- scr/screens/PokemonList.js
+++ scr/screens/PokemonList.js
@@ -1,77 +1,20 @@
-import React, { useState } from "react";
-import {
-  Image,
-  Pressable,
-  StyleSheet,
-  View,
-  FlatList,
-  TextInput,
-} from "react-native";
-import Icon from "react-native-vector-icons/Feather";
-import COLORS from "../constants/COLORS";
-import { useQuery } from "@apollo/client";
-import { getPokemon } from "../queries/getPokemon";
-import PokemonCard from "../components/PokemonCard";
-import { SPACE_LARGE, SPACE_MEDIUM } from "../constants/layouts";
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-const PokemonList = ({ navigation }) => {
-  const { loading, error, data } = useQuery(getPokemon, {
-    variables: {
-      name: "bul",
-    },
-  });
-  // console.log({ data, name: data?.pokemon_v2_pokemon?.[0].name });
-  const [text, setText] = useState("");
-
+const PokemonDetails = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.searchContainer}>
-        <TextInput style={styles.search} onChangeText={setText} value={text} />
-        <Icon name="search" color={COLORS.header} style={styles.icon} />
-      </View>
-
-      <FlatList
-        style={styles.flatList}
-        contentContainerStyle={styles.contentFlatList}
-        numColumns={2}
-        data={data?.pokemon_v2_pokemon || []}
-        renderItem={({ item }) => (
-          <PokemonCard item={item} navigation={navigation} />
-        )}
-        initialNumToRender={10}
-        ListHeaderComponentStyle={{ flexWrap: "wrap" }}
-        keyExtractor={(item) => item?.id}
-      />
+    <View>
+      <Pressable
+        onPress={() => {
+          navigation.navigate("PokemonDetails");
+        }}
+      >
+        <Text>Poke</Text>
+      </Pressable>
     </View>
   );
 };
 
-export default PokemonList;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  searchContainer: {
-    paddingHorizontal: SPACE_LARGE,
-    marginTop: SPACE_LARGE,
-    justifyContent: "center",
-  },
-  search: {
-    borderWidth: 2,
-    borderColor: COLORS.header,
-    borderRadius: 40,
-    height: 46,
-    paddingHorizontal: 20,
-  },
-  icon: {
-    position: "absolute",
-    right: SPACE_LARGE * 2,
-    fontSize: 20,
-  },
-  flatList: {
-    flex: 1,
-  },
-  contentFlatList: {
-    padding: SPACE_MEDIUM,
-  },
-});
+export default PokemonDetails;
+
+const styles = StyleSheet.create({});
